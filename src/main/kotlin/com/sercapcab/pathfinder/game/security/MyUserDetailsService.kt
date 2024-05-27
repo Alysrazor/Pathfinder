a202db50101a0a7edab30d758fb959ab04778bf2
Alysrazor <sergiocabades131999@gmail.com>
2024-05-27 15:47:36 +0200

    Borrar librerías que no se usan en el archivo.

--- src/main/kotlin/com/sercapcab/pathfinder/game/security/MyUserDetailsService.kt
+++ src/main/kotlin/com/sercapcab/pathfinder/game/security/MyUserDetailsService.kt
@@ -1,7 +1,6 @@
 package com.sercapcab.pathfinder.game.security
 
 import com.sercapcab.pathfinder.game.entity.account.AccountRepository
-import com.sercapcab.pathfinder.game.exceptions.account.AccountNotFoundException
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.userdetails.User
