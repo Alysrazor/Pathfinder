e9cd354119800829702dd4ec986a9177be53c0af
Alysrazor <sergiocabades131999@gmail.com>
2024-05-28 15:56:43 +0200

    Se ha añadido el certificado de Pathfinder

--- src/main/kotlin/com/sercapcab/pathfinder/game/security/ssl/WebClientService.kt
+++
@@ -1,8 +0,0 @@
-package com.sercapcab.pathfinder.game.security.ssl
-
-import org.springframework.boot.autoconfigure.web.reactive.function.client.WebClientSsl
-import org.springframework.stereotype.Service
-
-@Service
-class WebClientService(webClientBuilder: WebClientSsl) {
-}
